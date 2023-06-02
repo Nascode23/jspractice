ae9800563e235c1ad3c146adf07214b07fe31f7a
Nasreen <nihalia2@gmail.com>
2023-06-02 15:19:27 -0400

    if statement

---
+++ index.js
@@ -0,0 +1,57 @@
+//let x=12;
+//console.log(x);
+
+//let a=78;
+//if (x===12 && a===78) 
+ //   {let sum=x+a;
+ //   console.log(sum);
+//}
+//let age=17;
+//if(age>=18){
+    //do this
+   // console.log("access granted");
+//}
+//else{
+    //do this
+    //console.log("access denied");}
+//let wood=2
+0
+ //if (wood>40){
+ //   console.log("Let's build a treehouse");
+ //}
+ //else{
+ //   console.log("We don't have enough wood to build a treehouse")}
+//let username=admin
+//let password=Pass614
+// if (username="admin") {
+  //  console.log("Enter password")}
+//   else if(password="Pass614"){
+ //           console.log("access granted")}
+ //           else  {
+   //         console.log("access denied")}
+     
+   //             console.log("incorrect username log in again")}
+
+ //let num=456
+ //if  (num<0 ){
+  //  console.log ("negative")}
+  //  else if(num>0 && num>=100){
+  //  console.log("positive and more than 100")}
+  //  else if(num>0 && num<100){
+  //  console.log ("positive but less than 100")}
+
+  let num=56
+  if (num>=80)
+  {console.log("You did a good job")}
+     else if(num>=70){
+        console.log("keep trying")
+     }
+     else if(num>=60){
+        console.log( "ehh")}
+        else if(num>=55){
+            console.log("not to good")     
+        }
+     else if(num<55){
+        console.log("bad grade")
+     }
+
